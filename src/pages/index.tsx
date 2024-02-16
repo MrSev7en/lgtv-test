806b2c4dc91b6dd9123160d73feb09ed5f878e01
Eduardo <dueduardo01@hotmail.com>
2024-02-16 15:21:48 -0300

    Update index.tsx 4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   useEffect(() => {
     if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-      window.document.addEventListener('DOMContentLoaded', () => {
-        window.document.getElementById('button').addEventListener('click', () => {
-          window.document.write('<p>Hello</p>')
+      (window as any)?.document.addEventListener('DOMContentLoaded', () => {
+        (window as any)?.document.getElementById('button').addEventListener('click', () => {
+          (window as any)?.document.write('<p>Hello</p>')
         })
       })
     }
