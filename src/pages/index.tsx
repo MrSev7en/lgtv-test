24bdba3788ba14da2c1807cc041b5b3210a8fe18
Eduardo <dueduardo01@hotmail.com>
2024-02-16 15:18:02 -0300

    Update index.tsx 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,21 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+      document.addEventListener('DOMContentLoaded', () => {
+        document.getElementById('button').addEventListener('click', () => {
+          document.write('<p>Hello</p>');
+        });
+      });
+    }
+  }, []);
+  
   return (
     <main
       className={inter.className}
     >
-      <button onClick={() => document.write('<p>Hello</p>')}>Salve</button>
+      <button id="button">Salve</button>
     </main>
   )
 }
